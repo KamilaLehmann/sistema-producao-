a6b41d58b54253bf56053bb134a9712cf066f51d
Kamlila Lehmann <kamilamoraeslehmann@gmail.com>
2026-09-11 10:03:29 -0300

    Update sistema_varejo.py

--- sistema_varejo.py
+++ sistema_varejo.py
@@ -309,7 +309,7 @@ DEFAULT_EQUIPE = {
         {"nome": "Paula Roberta", "alias_excel": "PAULA ROBERTA SANTOS DA SILVA"},
         {"nome": "Weliton"},
         {"nome": "Ellen Kelly"},
-       {"nome": "EloizeM "},
+       {"nome": "eloizem"},
     ],
 }
 
